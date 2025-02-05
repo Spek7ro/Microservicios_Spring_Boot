4d28db0515aa7d37df4d7c76d436be872cd1ff07
Cristian <71907925+Spek7ro@users.noreply.github.com>
2025-02-04 22:57:02 -0600

    add entity Customer

--- Microservicios/customer/src/main/java/com/paymentchain/customer/CustomerApplication.java
+++ Microservicios/customer/src/main/java/com/paymentchain/customer/CustomerApplication.java
@@ -3,11 +3,13 @@ package com.paymentchain.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class CustomerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
+
+
 
     }
 }
