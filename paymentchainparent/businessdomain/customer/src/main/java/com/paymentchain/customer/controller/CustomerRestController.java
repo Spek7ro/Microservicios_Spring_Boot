bf6ab6106e3e6e6b74550376d1b33b4b6a2abd2f
Cristian <71907925+Spek7ro@users.noreply.github.com>
2025-04-04 18:42:50 -0600

    configuracion del cliente reactivo WebClient para REST en customer

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -1,15 +1,25 @@
 package com.paymentchain.customer.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.paymentchain.customer.entities.Customer;
 import com.paymentchain.customer.repository.CustomerRepository;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
+import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/customer")
@@ -17,6 +27,34 @@ public class CustomerRestController {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    private final WebClient.Builder webClientBuilder;
+
+    // Constructor para inyectar el WebClient.Builder
+    public CustomerRestController(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    // webclient requires HttpClient library to work properly
+    HttpClient client = HttpClient.create()
+            // Espera 15 segundos como máximo para establecer la conexión.
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 15000)
+            // Activa el keep-alive, que mantiene la conexión abierta en lugar de cerrarla después de cada solicitud.
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            // Configura el tiempo de espera de inactividad de la conexión.
+            .option(EpollChannelOption.TCP_KEEPIDLE, 300)
+            // Si no hay respuesta al paquete de keep-alive, se vuelve a intentar cada 60 segundos.
+            .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+            // Espera 1 segundo máximo para obtener una respuesta del servidor después de haber enviado la solicitud.
+            .responseTimeout(Duration.ofSeconds(1))
+            // Cuando la conexión se establece, se agregan dos handlers de timeout.
+            .doOnConnected(connection -> {
+                // Si tarda más de 5 segundos en leer la respuesta, lanza error.
+                connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+                // Si tarda más de 5 segundos en enviar la solicitud, lanza error.
+                connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+            }
+    );
 
     @Operation(summary = "Lista de clientes")
     @GetMapping()
@@ -73,4 +111,17 @@ public class CustomerRestController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    public String getProductName(long id) {
+        WebClient build = webClientBuilder.clientConnector(new ReactorClientHttpConnector(client))
+                .baseUrl("http://localhost:8083/product")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8083/product"))
+                .build();
+        JsonNode block = build.method(HttpMethod.GET).uri("/" + id)
+                .retrieve().bodyToMono(JsonNode.class).block();
+        assert block != null;
+        return block.get("name").asText();
+    }
+
 }
