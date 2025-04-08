86486bcdb51dcf7fca08325291971599b077f40c
Cristian <71907925+Spek7ro@users.noreply.github.com>
2025-04-07 22:11:46 -0600

    add TransactionController methods put, getById and Delete

---
+++ paymentchainparent/businessdomain/transactions/src/main/java/com/paymentchain/transactions/controllers/TransactionRestController.java
@@ -0,0 +1,66 @@
+package com.paymentchain.transactions.controllers;
+
+import com.paymentchain.transactions.entities.Transaction;
+import com.paymentchain.transactions.repository.TransactionRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionRestController {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Operation(summary = "Lista de transacciones")
+    @GetMapping
+    public List<Transaction> findAll() {
+        return transactionRepository.findAll();
+    }
+
+    @Operation(summary = "Crear una transacción")
+    @PostMapping
+    public ResponseEntity<Transaction> post(@RequestBody @Valid Transaction tx) {
+        return ResponseEntity.ok(transactionRepository.save(tx));
+    }
+
+    @Operation(summary = "Obtener una transacción por ID")
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getById(@PathVariable (name = "id") long id) {
+        Optional<Transaction> transaction = transactionRepository.findById(id);
+        if (transaction.isPresent()) {
+            return new ResponseEntity<>(transaction.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Operation(summary = "Actualizar una transacción")
+    @PutMapping("/{id}")
+    public ResponseEntity<Transaction> put(@PathVariable (name = "id") long id, @RequestBody @Valid Transaction tx) {
+        return transactionRepository.findById(id).map(existing -> {
+            tx.setId(id); // Asegurar que se actualiza el registro correcto
+            return ResponseEntity.ok(transactionRepository.save(tx));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @Operation(summary = "Eliminar una transacción por ID")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Transaction> delete(@PathVariable (name = "id") long id) {
+        Optional<Transaction> transaction = transactionRepository.findById(id);
+        if (transaction.isPresent()) {
+            transactionRepository.delete(transaction.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
